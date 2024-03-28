63554ac16ae1ad9f09cdbe554b1a0b98a2f8ef26
TheronBoozer <theronboozer@westminster.net>
2024-03-28 11:36:53 -0400

    functional 5 minute emails

--- Looper.py
+++ Looper.py
@@ -1,82 +1,22 @@
+import asyncio
 
-# dedicate times every --fifteen-- thirty minutes
-from asyncio import tasks
-import datetime
-import random
-
-from main import email
-
-recipient = 'bspatrick@wpi.edu'
-
-utc = datetime.timezone.utc
-times = [
-  datetime.time(hour=0, minute=0, tzinfo=utc),
-  datetime.time(hour=0, minute=30, tzinfo=utc),
-  datetime.time(hour=1, minute=0, tzinfo=utc),
-  datetime.time(hour=1, minute=30, tzinfo=utc),
-  datetime.time(hour=2, minute=0, tzinfo=utc),
-  datetime.time(hour=2, minute=30, tzinfo=utc),
-  datetime.time(hour=3, minute=0, tzinfo=utc),
-  datetime.time(hour=3, minute=30, tzinfo=utc),
-  datetime.time(hour=4, minute=0, tzinfo=utc),
-  datetime.time(hour=4, minute=30, tzinfo=utc),
-  datetime.time(hour=5, minute=0, tzinfo=utc),
-  datetime.time(hour=5, minute=30, tzinfo=utc),
-  datetime.time(hour=6, minute=0, tzinfo=utc),
-  datetime.time(hour=6, minute=30, tzinfo=utc),
-  datetime.time(hour=7, minute=0, tzinfo=utc),
-  datetime.time(hour=7, minute=30, tzinfo=utc),
-  datetime.time(hour=8, minute=0, tzinfo=utc),
-  datetime.time(hour=8, minute=30, tzinfo=utc),
-  datetime.time(hour=9, minute=0, tzinfo=utc),
-  datetime.time(hour=9, minute=30, tzinfo=utc),
-  datetime.time(hour=10, minute=0, tzinfo=utc),
-  datetime.time(hour=10, minute=30, tzinfo=utc),
-  datetime.time(hour=11, minute=0, tzinfo=utc),
-  datetime.time(hour=11, minute=30, tzinfo=utc),
-  datetime.time(hour=12, minute=0, tzinfo=utc),
-  datetime.time(hour=12, minute=30, tzinfo=utc),
-  datetime.time(hour=13, minute=0, tzinfo=utc),
-  datetime.time(hour=13, minute=30, tzinfo=utc),
-  datetime.time(hour=14, minute=0, tzinfo=utc),
-  datetime.time(hour=14, minute=30, tzinfo=utc),
-  datetime.time(hour=15, minute=0, tzinfo=utc),
-  datetime.time(hour=15, minute=30, tzinfo=utc),
-  datetime.time(hour=16, minute=0, tzinfo=utc),
-  datetime.time(hour=16, minute=30, tzinfo=utc),
-  datetime.time(hour=17, minute=0, tzinfo=utc),
-  datetime.time(hour=17, minute=30, tzinfo=utc),
-  datetime.time(hour=18, minute=0, tzinfo=utc),
-  datetime.time(hour=18, minute=30, tzinfo=utc),
-  datetime.time(hour=19, minute=0, tzinfo=utc),
-  datetime.time(hour=19, minute=30, tzinfo=utc),
-  datetime.time(hour=20, minute=0, tzinfo=utc),
-  datetime.time(hour=20, minute=30, tzinfo=utc),
-  datetime.time(hour=21, minute=0, tzinfo=utc),
-  datetime.time(hour=21, minute=30, tzinfo=utc),
-  datetime.time(hour=22, minute=0, tzinfo=utc),
-  datetime.time(hour=22, minute=30, tzinfo=utc),
-  datetime.time(hour=23, minute=0, tzinfo=utc),
-  datetime.time(hour=23, minute=30, tzinfo=utc),
-]
+from main import random_email
 
 
-subjects = [
-    'pretty please',
-    'nominations',
-    'reply',
-    'Brooooooooke',
-    'why do you do this to me',
-]
+async def periodic():
+    while True:
+        print('periodic')
+        await random_email()
+        await asyncio.sleep(5*60)
 
-bodies = [
-    ''
-]
+def stop():
+    task.cancel()
 
-# big daddy loop that runs every fifteen minutes
-@tasks.loop(time=times)
-async def Big_loop():
-    subject = subjects[int(random.random()*len(subjects))]
-    body = bodies[int(random.random()*len(bodies))]
+loop = asyncio.get_event_loop()
+# loop.call_later(5, stop)
+task = loop.create_task(periodic())
 
-    await email(recipient, subject, body)
+try:
+    loop.run_until_complete(task)
+except asyncio.CancelledError:
+    pass
--- main.py
+++ main.py
@@ -1,7 +1,32 @@
+import random
 import win32com.client as win32
 
 outlook = win32.Dispatch('outlook.application')
 
+# email variable setup --
+# the email it sends to and the body and subject options
+recipient = 'wtboozer@wpi.edu' #'bspatrick@wpi.edu'
+
+subjects = [
+    'pretty please',
+    'nominations',
+    'reply',
+    'Brooooooooke',
+    'why do you do this to me',
+]
+
+bodies = [
+    'What are the chances you even read these?',
+    'Brooke '
+]
+
+async def random_email():
+    subject = subjects[int(random.random()*len(subjects))]
+    body = bodies[int(random.random()*len(bodies))]
+
+    await email(recipient, subject, body)
+    
+    
 async def email(recipient, subject, body):
     mail = outlook.CreateItem(0)
     mail.To = recipient
